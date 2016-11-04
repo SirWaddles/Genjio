1800c92e84db3f616588a9b1efbcff8c038a888c
Timothy Clissold <sirwaddlesworth06@gmail.com>
2016-11-04 12:52:53 +1100

    added multiple templates and masking

--- videocheck.cpp
+++ videocheck.cpp
@@ -2,13 +2,77 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include "videocheck.h"
+#include <Windows.h>
+
+#define FRAME_MULTIPLIER 10.0
 
 void SendProgressMessage(WSserver* server, websocketpp::connection_hdl hdl, std::string message) {
 	server->send(hdl, message.c_str(), message.length(), websocketpp::frame::opcode::text);
 }
 
+VideoAnalysisTask::VideoAnalysisTask(WSserver* server, websocketpp::connection_hdl hdl, std::string message) : Task(server, hdl, message), targetTemplate(emptyMat) {
+
+}
+
 void VideoAnalysisTask::Run() {
 	VideoCheck();
+	foundTemplate = false;
+}
+
+ImageTemplate::ImageTemplate() {
+
+}
+
+ImageTemplate::ImageTemplate(int width, int height) : image(height, width, CV_8UC3), mask(height, width, CV_8UC3){
+
+}
+
+void VideoAnalysisTask::CreateTemplates(double frameWidth, double frameHeight) {
+	static const std::string templatePaths[] = {"bedouin.png", "chrome.png", "cinnabar.png", "malachite.png", "nihon.png", "nomad.png", "orchite.png", "young.png"};
+	const int templateCount = 8;
+	for (int i = 0; i < templateCount; i++) {
+		std::string path = std::string("templates/") + templatePaths[i];
+		cv::Mat templateImage = cv::imread(path, cv::IMREAD_UNCHANGED);
+		double tWidth = templateImage.cols;
+		double tHeight = templateImage.rows;
+		double tgtWidth = (tWidth / 3840.0) * frameWidth;
+		double tgtHeight = (tHeight / 2160.0) * frameHeight;
+		cv::Mat result;
+		cv::resize(templateImage, result, cv::Size(tgtWidth, tgtHeight), 0, 0);
+		
+		ImageTemplate fResult(tgtWidth, tgtHeight);
+
+		cv::Mat out[] = { fResult.image, fResult.mask };
+		cv::Mat in[] = { result };
+		int from_to[] = { 0,0, 1,1, 2,2, 3,3, 3,4, 3,5 };
+		cv::mixChannels(in, 1, out, 2, from_to, 6);
+
+		templates.push_back(fResult);
+	}
+}
+
+bool VideoAnalysisTask::MatchTemplates(cv::Mat& image) {
+	if (foundTemplate) {
+		return MatchTemplate(image, targetTemplate);
+	}
+
+	for (auto&& i : templates) {
+		if (MatchTemplate(image, i)) {
+			targetTemplate = i;
+			foundTemplate = true;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool VideoAnalysisTask::MatchTemplate(cv::Mat& image, ImageTemplate& templateImage) {
+	cv::Mat result;
+	cv::matchTemplate(image, templateImage.image, result, cv::TM_SQDIFF, templateImage.mask);
+	double minVal, maxVal;
+	cv::minMaxLoc(result, &minVal, &maxVal, nullptr, nullptr);
+	printf("Match: %f\n", minVal);
+	return minVal < 200.0;
 }
 
 void VideoAnalysisTask::VideoCheck() {
@@ -20,39 +84,27 @@ void VideoAnalysisTask::VideoCheck() {
 		printf("Closed\n");
 	}
 
-	double frames = capture.get(cv::CAP_PROP_FRAME_COUNT) / 10.0;
+	double frames = capture.get(cv::CAP_PROP_FRAME_COUNT) / FRAME_MULTIPLIER;
 	double fps = capture.get(cv::CAP_PROP_FPS);
 	double frameWidth = capture.get(cv::CAP_PROP_FRAME_WIDTH);
 	double frameHeight = capture.get(cv::CAP_PROP_FRAME_HEIGHT);
-	cv::Mat templateImage = cv::imread("template.png");
-	double templateWidth = templateImage.cols;
-	double templateHeight = templateImage.rows;
 
-	double tgtTmplWidth = (templateWidth / 1920.0) * frameWidth; // Template is designed for 1080p
-	double tgtTmplHeight = (templateHeight / 1080.0) * frameHeight; // This will only work for 16:9 resolutions
-	cv::Mat tgtTemplate;
-	cv::resize(templateImage, tgtTemplate, cv::Size(tgtTmplWidth, tgtTmplHeight), 0, 0);
+	CreateTemplates(frameWidth, frameHeight);
+
 	cv::Mat image;
 	int count = 0;
 	int foundFrames = 0;
+
 	for (;;) {
 		count++;
-		capture.set(cv::CAP_PROP_POS_FRAMES, double(count) * 10);
+		capture.set(cv::CAP_PROP_POS_FRAMES, double(count) * FRAME_MULTIPLIER);
 		capture >> image;
 		if (image.empty()) break;
 		cv::Rect region(0, (frameHeight * 0.7), frameWidth * 0.3, frameHeight * 0.3);
 		cv::Mat croppedImage(image, region);
-		cv::Mat result;
-		cv::matchTemplate(croppedImage, tgtTemplate, result, cv::TM_CCOEFF);
-
-		double minVal, maxVal;
-		cv::Point minLoc, maxLoc;
-		cv::minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc);
-		bool foundFrame = false;
-		if (maxVal > 60000000.0) { // 60m is pretty confident
-			foundFrames++;
-			foundFrame = true;
-		}
+		
+		bool foundFrame = MatchTemplates(croppedImage);
+		if (foundFrame) foundFrames++;
 
 		char buffer[500]; // this is dumb
 		int length = sprintf(buffer, "{\"progress\": %f,\"frame\":%d,\"genji\":%s}", double(count) / frames, count, foundFrame ? "true" : "false");
--- videocheck.h
+++ videocheck.h
@@ -1,11 +1,28 @@
 #pragma once
 
+#include <opencv2/imgproc.hpp>
+#include <vector>
 #include "task.h"
+
+struct ImageTemplate {
+	cv::Mat image;
+	cv::Mat mask;
+	ImageTemplate();
+	ImageTemplate(int width, int height);
+};
 
 class VideoAnalysisTask : public Task {
 public:
-	using Task::Task;
+	VideoAnalysisTask(WSserver* server, websocketpp::connection_hdl hdl, std::string message);
 
 	virtual void Run();
 	void VideoCheck();
+	void CreateTemplates(double frameWdith, double frameHeight);
+	bool MatchTemplate(cv::Mat& image, ImageTemplate& img);
+	bool MatchTemplates(cv::Mat& image);
+private:
+	std::vector<ImageTemplate> templates;
+	bool foundTemplate;
+	ImageTemplate& targetTemplate;
+	ImageTemplate emptyMat; // hacks? How else do I do this?
 };
